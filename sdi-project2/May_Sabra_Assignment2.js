ae06b7111aaaa71a67684f6417d6f02786ace5a9
gooskey <sabramay@fullsail.edu>
2013-04-15 16:37:44 -0600

    Added .js file

---
+++ sdi-project2/May_Sabra_Assignment2.js
@@ -0,0 +1,43 @@
+//alert("JavaScript works!");
+//Sabra May
+//April 18, 2013
+//SDI 1304
+//Assignment 2
+
+
+
+
+// Procedure
+	var kidCloset = function(hours) {
+		console.log("I need to clean out my son's closet. I wonder how much time I have.");
+		if(hours >= 3) {
+			console.log("Looks like I have 3 free hours to finish before my kids come home.")
+		}else{
+			if(hours < 3 && hours >=1){
+				console.log("I have less than 3 hours. I will have to start now and finish later.")
+			}else{
+				consoel.log("I don't have enough time right now, I will have to do it another day.")
+			}
+		}
+	};
+	kidCloset(3);
+	
+//Boolean Function
+
+
+
+
+
+//Number Function
+
+
+
+
+
+//String Function
+
+
+
+
+
+//Array Function
