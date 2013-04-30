5921918859e1e7bf8d47c3bac64dd7fc246928a7
gooskey <sabramay@fullsail.edu>
2013-04-29 19:27:46 -0600

    Added Validate Phone Number Function

---  May_Sabra_Assignment4/May_Sabra_Assignment4.js
+++  May_Sabra_Assignment4/May_Sabra_Assignment4.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+//Sabra May
+//SDI 1304
+//Assignment 4
+
+//String Functions
+	//Validate phone number
+	function phoneNumber(myNumber) {
+		var numberCheck = /^(\d{3})-(\d{3})-(\d{4})$/;
+		if (myNumber.match(numberCheck)) {
+			numWorks = myNumber + " is valid.";
+			return numWorks;
+		}else {
+			notWork = "The number " + myNumber + " is not valid";
+			return notWork;
+		}
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//Calling all Functions!
+	//Validate Phone Number
+	var theNumber = phoneNumber(801-555-5555);
+	console.log(theNumber);	
